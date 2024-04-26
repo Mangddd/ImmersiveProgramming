9d26fab3ee04f0b4e44ec99a3bbd532ec44aa058
ashpopo <lej112026@gmail.com>
2024-04-26 21:10:12 +0900

    게임 성공시 넘어가기

--- Chito's_Flaming_Torch/Assets/Scripts/UIManager.cs
+++ Chito's_Flaming_Torch/Assets/Scripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     public Image[] heads; // UI 이미지 배열, 이름을 'heads'에서 'heads'로 유지
     private int currentHead = 0; // 현재 채워진 이미지 인덱스
+    public Button restartButton; // 게임 재시작 버튼
 
     // 오브젝트가 터치될 때 호출되는 메서드
     public void UpdateHearts()
@@ -16,5 +17,11 @@ public class UIManager : MonoBehaviour
             heads[currentHead].color = Color.white; // 이미지 색상 변경
             currentHead++; // 다음 이미지로 이동
         }
+        if (currentHead == 3){
+                            
+             restartButton.gameObject.SetActive(true); // 게임 재시작 버튼 활성화
+     }
     }
+    
+
 }
