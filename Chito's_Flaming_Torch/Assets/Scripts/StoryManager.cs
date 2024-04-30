e682601633947ff18a273814ef1f2b88f2c53ddf
Chaewon Kim <162666695+kk02ev2@users.noreply.github.com>
2024-04-30 17:57:55 +0900

    talkmanager_del

--- Chito's_Flaming_Torch/Assets/Scripts/StoryManager.cs
+++ Chito's_Flaming_Torch/Assets/Scripts/StoryManager.cs
@@ -14,7 +14,7 @@ public class StoryManager : MonoBehaviour
     public Button nextButton; // 다음으로 진행할 버튼
 
     public Sprite[] characterSprites; // 각 스토리 단계별로 사용할 캐릭터 이미지 배열
-    public string[] storyMessages; // 각 스토리 단계별로 사용할 텍스트 배열
+    public string[] storyMessages;
 
     private int currentStoryIndex = 0; // 현재 스토리 인덱스
 
@@ -34,17 +34,14 @@ public class StoryManager : MonoBehaviour
     // 스토리 진행 함수
     public void ProgressStory()
     {
-
-        // 다음 스토리 단계로 진행
         currentStoryIndex++;
+
         // 현재 스토리 인덱스가 배열 범위를 벗어나면 종료
         if (currentStoryIndex >= storyMessages.Length)
         {
-            // 추가적인 처리나 다음 동작을 여기에 추가할 수 있습니다.
-            Debug.Log("스토리 끝!");
+            //Debug.Log("스토리 끝!");
             // 버튼 활성화 여부 결정 (마지막 스토리 단계에서는 버튼 비활성화)
             nextButton.gameObject.SetActive(true);
-
             return;
         }
 
